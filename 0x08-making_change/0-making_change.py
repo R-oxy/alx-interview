63ff169dae21d1a6199192f606cf17c6cb2b0215
R-oxy <aronmangati@gmail.com>
2024-07-24 08:58:46 +0000

    making changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module to determine the minimum number of coins required to
+meet a given amount.
+"""
+
+
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet total.
+    :param coins: List of coin denominations.
+    :param total: The total amount to achieve with the coins.
+    :return: Fewest number of coins needed or -1 if not possible.
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
